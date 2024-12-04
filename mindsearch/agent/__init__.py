22d89ebe1dd0f6077a3e690f05e7ef92ad79b667
braisedpork1964 <497494458@qq.com>
2024-12-04 10:04:23 +0000

    update tool protocols

--- mindsearch/agent/__init__.py
+++ mindsearch/agent/__init__.py
@@ -25,9 +25,7 @@ from .mindsearch_prompt import (
 LLM = {}
 
 
-def init_agent(
-    lang="cn", model_format="internlm_server", search_engine="BingSearch", use_async=False
-):
+def init_agent(lang="cn", model_format="internlm_server", search_engine="BingSearch", use_async=False):
     mode = "async" if use_async else "sync"
     llm = LLM.get(model_format, {}).get(mode)
     if llm is None:
@@ -35,52 +33,50 @@ def init_agent(
         if llm_cfg is None:
             raise NotImplementedError
         if use_async:
-            cls_name = (
-                llm_cfg["type"].split(".")[-1]
-                if isinstance(llm_cfg["type"], str)
-                else llm_cfg["type"].__name__
-            )
+            cls_name = llm_cfg["type"].split(".")[-1] if isinstance(llm_cfg["type"], str) else llm_cfg["type"].__name__
             llm_cfg["type"] = f"lagent.llms.Async{cls_name}"
         llm = create_object(llm_cfg)
         LLM.setdefault(model_format, {}).setdefault(mode, llm)
 
     date = datetime.now().strftime("The current date is %Y-%m-%d.")
     plugins = [
-        dict(
-            type=AsyncWebBrowser if use_async else WebBrowser,
-            searcher_type=search_engine,
-            topk=6,
-            secret_id=os.getenv("TENCENT_SEARCH_SECRET_ID"),
-            secret_key=os.getenv("TENCENT_SEARCH_SECRET_KEY"),
-        )
-        if search_engine == "TencentSearch"
-        else dict(
-            type=AsyncWebBrowser if use_async else WebBrowser,
-            searcher_type=search_engine,
-            topk=6,
-            api_key=os.getenv("WEB_SEARCH_API_KEY"),
+        (
+            dict(
+                type=AsyncWebBrowser if use_async else WebBrowser,
+                searcher_type=search_engine,
+                topk=6,
+                secret_id=os.getenv("TENCENT_SEARCH_SECRET_ID"),
+                secret_key=os.getenv("TENCENT_SEARCH_SECRET_KEY"),
+            )
+            if search_engine == "TencentSearch"
+            else dict(
+                type=AsyncWebBrowser if use_async else WebBrowser,
+                searcher_type=search_engine,
+                topk=6,
+                api_key=os.getenv("WEB_SEARCH_API_KEY"),
+            )
         )
     ]
     agent = (AsyncMindSearchAgent if use_async else MindSearchAgent)(
         llm=llm,
         template=date,
         output_format=InterpreterParser(
-            template=GRAPH_PROMPT_CN if lang == "cn" else GRAPH_PROMPT_EN
+            begin="<|action_start|><|interpreter|>",
+            end="<|action_end|>",
+            template=GRAPH_PROMPT_CN if lang == "cn" else GRAPH_PROMPT_EN,
         ),
         searcher_cfg=dict(
             llm=llm,
             plugins=plugins,
             template=date,
             output_format=PluginParser(
+                begin="<|action_start|><|plugin|>",
+                end="<|action_end|>",
                 template=searcher_system_prompt_cn if lang == "cn" else searcher_system_prompt_en,
                 tool_info=get_plugin_prompt(plugins),
             ),
-            user_input_template=(
-                searcher_input_template_cn if lang == "cn" else searcher_input_template_en
-            ),
-            user_context_template=(
-                searcher_context_template_cn if lang == "cn" else searcher_context_template_en
-            ),
+            user_input_template=(searcher_input_template_cn if lang == "cn" else searcher_input_template_en),
+            user_context_template=(searcher_context_template_cn if lang == "cn" else searcher_context_template_en),
         ),
         summary_prompt=FINAL_RESPONSE_CN if lang == "cn" else FINAL_RESPONSE_EN,
         max_turn=10,
